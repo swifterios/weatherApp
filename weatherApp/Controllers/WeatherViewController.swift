a026e338d46bb5410d2c1f6915d07954406432a5
Vladislav <lordroga2@gmail.com>
2021-08-13 10:21:05 +0300

    struct of  the app

--- weatherApp/Controllers/WeatherViewController.swift
+++ weatherApp/Controllers/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  weatherApp
 //
 //  Created by Владислав on 13.08.2021.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
---
+++ weatherApp/NetworkManager/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  weatherApp
+//
+//  Created by Владислав on 13.08.2021.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    
+}
