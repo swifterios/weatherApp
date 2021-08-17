b6f2bf1772e031efe628cd48cb08183a03d5a3ca
Vladislav <lordroga2@gmail.com>
2021-08-17 12:23:13 +0300

    display hour added collection for images

--- weatherApp/Controllers/WeatherViewController.swift
+++ weatherApp/Controllers/WeatherViewController.swift
@@ -8,22 +8,70 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
+    var weatherData: Weather?
+    
+    //MARK: - Outlets
+    
+    @IBOutlet var hoursLabels: [UILabel]!
+    
+    @IBOutlet var weatherImages: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getWeather()
     }
 
-
+    //MARK: - Using api
+    
     func getWeather() {
         NetworkManager.shared.getWeather { [weak self] result in
             switch result {
             case .success(let model):
-                print(model)
+                self?.weatherData = model
+                self?.updateHoursLabels()
             case .failure(let error):
                 print(error)
             }
+        }
+    }
+    
+    
+    //MARK: - Update UI
+    
+    func updateHoursLabels() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            guard let weatherData = self.weatherData else {
+                return
+            }
+            
+            let calendar = Calendar.current
+            
+            var currentHour: Int = {
+                let currentHour = calendar.component(.hour, from: Date())
+                return currentHour
+            }()
+            var currentDay = 0
+            
+            for index in 0...23 {
+                if index == 0 {
+                    self.hoursLabels[index].text = "Сейчас"
+                } else {
+                    if currentHour >= 22 {
+                        currentHour = 0
+                        currentDay += 1
+                    }
+                    self.hoursLabels[index].text = weatherData.forecasts![currentDay].hours![currentHour].hour
+                    currentHour += 1
+                }
+            }
+                
+            
         }
     }
 }
--- weatherApp/NetworkManager/NetworkManager.swift
+++ weatherApp/NetworkManager/NetworkManager.swift
@@ -47,6 +47,5 @@ final class NetworkManager {
             }
         }
         task.resume()
-        
     }
 }
