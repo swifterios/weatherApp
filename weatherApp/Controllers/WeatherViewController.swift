2b583636265ab8535a1c4cc03f8d6f983d2ebfac
Vladislav <lordroga2@gmail.com>
2021-08-18 21:59:51 +0300

    display main info

--- weatherApp/Controllers/WeatherViewController.swift
+++ weatherApp/Controllers/WeatherViewController.swift
@@ -13,6 +13,10 @@ class WeatherViewController: UIViewController {
     
     //MARK: - Outlets
     
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var currentTemp: UILabel!
+    
     @IBOutlet var hoursLabels: [UILabel]!
     @IBOutlet var weatherImages: [UIImageView]!
     @IBOutlet var tempLabels: [UILabel]!
@@ -43,9 +47,11 @@ class WeatherViewController: UIViewController {
             switch result {
             case .success(let model):
                 self?.weatherData = model
+                self?.updateMainInfo()
                 self?.updateHoursHourWeather()
                 self?.updateWeatherByDay()
                 self?.updateInfoDay()
+                
             case .failure(let error):
                 print(error)
             }
@@ -54,6 +60,22 @@ class WeatherViewController: UIViewController {
     
     
     //MARK: - Update UI
+    
+    func updateMainInfo() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            guard let weatherData = self.weatherData else {
+                return
+            }
+            
+            self.countryLabel.text = String(weatherData.geo_object!.country!.name!) + " , " + String(weatherData.geo_object!.province!.name!)
+            self.currentWeatherLabel.text = self.getWeatherName(weatherName: weatherData.fact!.condition!)
+            self.currentTemp.text = String(weatherData.fact!.temp!) + "°"
+        }
+    }
     
     func updateHoursHourWeather() {
         DispatchQueue.main.async { [weak self] in
@@ -230,5 +252,49 @@ class WeatherViewController: UIViewController {
         }
     }
     
+    func getWeatherName(weatherName: String) -> String {
+        switch weatherName {
+        case "clear":
+            return "Ясно"
+        case "partly-cloudy":
+            return "Малооблачно"
+        case "cloudy":
+            return "Облачно с прояснениями"
+        case "overcast":
+            return "Пасмурно"
+        case "drizzle":
+            return "Морось"
+        case "light-rain":
+            return "Небольшой дождь"
+        case "rain":
+            return "Дождь"
+        case "moderate-rain":
+            return "Умеренно сильный дождь"
+        case "heavy-rain":
+            return "Сильный дождь"
+        case "continuous-heavy-rain":
+            return "Длительно сильный дождь"
+        case "showers":
+            return "Ливень"
+        case "wet-snow":
+            return "Дождь со снегом"
+        case "light-snow":
+            return "Небольшой снег"
+        case "snow":
+            return "Снег"
+        case "snow-showers":
+            return "Снегопад"
+        case "hail":
+            return "Град"
+        case "thunderstorm":
+            return "Гроза"
+        case "thunderstorm-with-rain":
+            return "Дождь с грозой"
+        case "thunderstorm-with-hail":
+            return "Гроза с градом"
+        default:
+            return ""
+        }
+    }
 }
 
